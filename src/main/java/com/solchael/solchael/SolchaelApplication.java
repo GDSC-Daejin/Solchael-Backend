3f562abd740e5868b1d36ac7a8b91a30032ea57e
Seung-9 <ssg9505fj22@naver.com>
2024-01-27 14:42:54 +0900

    초기 세팅

---
+++ src/main/java/com/solchael/solchael/SolchaelApplication.java
@@ -0,0 +1,13 @@
+package com.solchael.solchael;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolchaelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolchaelApplication.class, args);
+    }
+
+}
