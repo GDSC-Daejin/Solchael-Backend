fb354aabd92f8fb81a5a1ee0e82027eddb6c0bc9
Seung-9 <ssg9505fj22@naver.com>
2024-02-04 00:29:08 +0900

    chore: 로그인 @Valid 어노테이션 제거

--- src/main/java/com/solchael/solchael/dto/LoginRequest.java
+++ src/main/java/com/solchael/solchael/dto/LoginRequest.java
@@ -1,11 +1,16 @@
 package com.solchael.solchael.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
 public class LoginRequest {
+
+    @NotBlank(message = "아이디를 입력해주세요.")
     private String email;
+
+    @NotBlank(message = "비밀번호를 입력해주세요.")
     private String password;
 }
