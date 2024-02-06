2f069daec2dd963e77ac1f0451c196e92b43f1c5
Seung-9 <ssg9505fj22@naver.com>
2024-02-06 19:13:53 +0900

    feat: Storage 설정

---
+++ src/main/java/com/solchael/solchael/config/GoogleCloudStorageConfig.java
@@ -0,0 +1,33 @@
+package com.solchael.solchael.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+
+@Configuration
+public class GoogleCloudStorageConfig {
+
+    @Value("${API-KEY.projectId}")
+    private String project;
+
+    @Value("${API-KEY.resource}")
+    private String resourceJson;
+
+    @Bean
+    public Storage storage() throws IOException {
+
+        ClassPathResource resource = new ClassPathResource(resourceJson);
+        GoogleCredentials credentials = GoogleCredentials.fromStream(resource.getInputStream());
+        return StorageOptions.newBuilder()
+                .setProjectId(project)
+                .setCredentials(credentials)
+                .build()
+                .getService();
+    }
+}
