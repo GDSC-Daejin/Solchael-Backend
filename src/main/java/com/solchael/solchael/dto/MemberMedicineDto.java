bd3142db4dbb2dd96e6d5ec6b057f91cba2db4af
Seung-9 <ssg9505fj22@naver.com>
2024-02-08 23:26:41 +0900

    feat: 나의 약 관련 DTO

---
+++ src/main/java/com/solchael/solchael/dto/MemberMedicineDto.java
@@ -0,0 +1,30 @@
+package com.solchael.solchael.dto;
+
+import com.solchael.solchael.entity.MemberMedicine;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+public class MemberMedicineDto {
+
+    private Long id;
+    private String name;
+    private String image;
+    private LocalDateTime startTime;
+    private LocalDateTime expiration;
+
+    public static MemberMedicineDto fromEntity(MemberMedicine memberMedicine) {
+
+        return MemberMedicineDto.builder()
+                .id(memberMedicine.getMedicine().getId())
+                .name(memberMedicine.getMedicine().getName())
+                .image(memberMedicine.getMedicine().getItemImage())
+                .startTime(memberMedicine.getStartTime())
+                .expiration(memberMedicine.getEndTime())
+                .build();
+
+    }
+}
